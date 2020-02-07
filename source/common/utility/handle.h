a4914b5fc1eb1d24db843eec2b6aa71a4f390d63
Ben Self <gumgo1@gmail.com>
2020-02-07 09:12:35 -0800

    Add handle utility class

---
+++ source/common/utility/handle.h
@@ -0,0 +1,44 @@
+#ifndef WAVELANG_COMMON_UTILITY_HANDLE_H__
+#define WAVELANG_COMMON_UTILITY_HANDLE_H__
+
+#include "common/common.h"
+
+// A generic handle class wrapping simple data such as uint32. Handles are immutable once constructed and are type-safe
+// - they can only be compared with other handles of the same type. Example handle definition:
+// struct s_foo_handle_identifier {};
+// using h_foo = c_handle<s_foo_handle_identifier, uint32>;
+template<typename t_identifier, typename t_data, t_data k_invalid_data = static_cast<t_data>(-1)>
+class c_handle {
+public:
+	static c_handle construct(t_data data) {
+		c_handle result;
+		result.m_data = data;
+		return result;
+	}
+
+	static c_handle invalid() {
+		return construct(k_invalid_data);
+	}
+
+	bool is_valid() const {
+		return m_data != k_invalid_data;
+	}
+
+	t_data get_data() const {
+		return m_data;
+	}
+
+	bool operator==(const c_handle &other) const {
+		bool result = m_data == other.m_data;
+		return result;
+	}
+
+	bool operator!=(const c_handle &other) const {
+		return !(*this == other);
+	}
+
+private:
+	t_data m_data;
+};
+
+#endif // WAVELANG_COMMON_UTILITY_HANDLE_H__
